799cb3009306bafbb5ddece2668ef38f268c92c8
Hao Zhang <zhangghaoo@gmail.com>
2021-10-23 19:43:55 +0800

    Add init.sh

---
+++ scripts/cockroachdb/init.sh
@@ -0,0 +1,60 @@
+mkdir certs my-safe-directory
+
+cockroach cert create-ca \
+--certs-dir=certs \
+--ca-key=my-safe-directory/ca.key
+
+cockroach cert create-node \
+localhost \
+my_db \
+--certs-dir=certs \
+--ca-key=my-safe-directory/ca.key
+
+cockroach cert create-client \
+root \
+--certs-dir=certs \
+--ca-key=my-safe-directory/ca.key
+
+cockroach start \
+--certs-dir=certs \
+--store=node1 \
+--listen-addr=localhost:26257 \
+--http-addr=localhost:8080 \
+--join=localhost:26257,localhost:26258,localhost:26259,localhost:26260,localhost:26261 \
+--background
+
+cockroach start \
+--certs-dir=certs \
+--store=node2 \
+--listen-addr=localhost:26258 \
+--http-addr=localhost:8081 \
+--join=localhost:26257,localhost:26258,localhost:26259,localhost:26260,localhost:26261 \
+--background
+
+cockroach start \
+--certs-dir=certs \
+--store=node3 \
+--listen-addr=localhost:26259 \
+--http-addr=localhost:8082 \
+--join=localhost:26257,localhost:26258,localhost:26259,localhost:26260,localhost:26261 \
+--background
+
+cockroach start \
+--certs-dir=certs \
+--store=node4 \
+--listen-addr=localhost:26260 \
+--http-addr=localhost:8083 \
+--join=localhost:26257,localhost:26258,localhost:26259,localhost:26260,localhost:26261 \
+--background
+
+cockroach start \
+--certs-dir=certs \
+--store=node5 \
+--listen-addr=localhost:26261 \
+--http-addr=localhost:8084 \
+--join=localhost:26257,localhost:26258,localhost:26259,localhost:26260,localhost:26261 \
+--background
+
+cockroach init --certs-dir=certs --host=localhost:26257
+
+grep 'node starting' node1/logs/cockroach.log -A 11
