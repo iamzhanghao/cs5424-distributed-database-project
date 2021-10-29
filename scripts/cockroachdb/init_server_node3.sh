54074dad58f707b6873c2c4de7510ccbdd2f3f5a
Hao Zhang <zhangghaoo@gmail.com>
2021-10-29 23:59:31 +0800

    Set background false

--- scripts/cockroachdb/init_server_node3.sh
+++ scripts/cockroachdb/init_server_node3.sh
@@ -4,6 +4,6 @@ cockroach start \
 --listen-addr=xcnd32.comp.nus.edu.sg:26267 \
 --http-addr=localhost:8090 \
 --join=xcnd30.comp.nus.edu.sg:26267,xcnd31.comp.nus.edu.sg:26267,xcnd32.comp.nus.edu.sg:26267,xcnd33.comp.nus.edu.sg:26267,xcnd34.comp.nus.edu.sg:26267 \
---background
+#--background
 
 grep 'node starting' cockroach_store/node3/logs/cockroach.log -A 11
--- scripts/cockroachdb/init_server_node5.sh
+++ scripts/cockroachdb/init_server_node5.sh
@@ -4,6 +4,6 @@ cockroach start \
 --listen-addr=xcnd34.comp.nus.edu.sg:26267 \
 --http-addr=localhost:8090 \
 --join=xcnd30.comp.nus.edu.sg:26267,xcnd31.comp.nus.edu.sg:26267,xcnd32.comp.nus.edu.sg:26267,xcnd33.comp.nus.edu.sg:26267,xcnd34.comp.nus.edu.sg:26267 \
---background
+#--background
 
 grep 'node starting' cockroach_store/node5/logs/cockroach.log -A 11
