69add977a513eb31a158c146b85207f8de54a041
Hao Zhang <zhangghaoo@gmail.com>
2021-10-30 14:49:26 +0800

    Init cluster

--- scripts/cockroachdb/start_server.sh
+++ scripts/cockroachdb/start_server.sh
@@ -8,8 +8,17 @@ ssh cs4224c@${s}.comp.nus.edu.sg -n << EOF
   cd cs5424-distributed-database-project/;
   source .bash_profile;
   whoami;
-  echo ${s}.comp.nus.edu.sg
+  echo ${s}.comp.nus.edu.sg;
 
+  cockroach start \
+  --insecure \
+  --store=cockroach_store/node1 \
+  --listen-addr=${s}.comp.nus.edu.sg:26267 \
+  --http-addr=localhost:8090 \
+  --join=xcnd30.comp.nus.edu.sg:26267,xcnd31.comp.nus.edu.sg:26267,xcnd32.comp.nus.edu.sg:26267,xcnd33.comp.nus.edu.sg:26267,xcnd34.comp.nus.edu.sg:26267 \
+  --background;
 
 EOF
 done
+
+cockroach init --insecure --host=xcnd30.comp.nus.edu.sg:26267
