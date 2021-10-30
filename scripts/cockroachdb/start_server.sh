88d20a38550d8d987f1ec8dc4342809f096cd5f7
Hao Zhang <zhangghaoo@gmail.com>
2021-10-30 15:22:40 +0800

    Fix process hanging

--- scripts/cockroachdb/start_server.sh
+++ scripts/cockroachdb/start_server.sh
@@ -2,12 +2,18 @@ for s in xcnd30 xcnd31 xcnd32 xcnd33 xcnd34
 do
 ssh cs4224c@${s}.comp.nus.edu.sg -n "
   killall -9 cockroach;
-  exit;
 "
 done
 
 rm -rf cockroach_store;
 rm -rf cockroach-data;
+
+#cockroach start --insecure \
+#--store=cockroach_store/xcnd30 \
+#--listen-addr=xcnd30.comp.nus.edu.sg:26267 \
+#--http-addr=localhost:8090 \
+#--join=xcnd30.comp.nus.edu.sg:26267,xcnd31.comp.nus.edu.sg:26267,xcnd32.comp.nus.edu.sg:26267,xcnd33.comp.nus.edu.sg:26267,xcnd34.comp.nus.edu.sg:26267 \
+#--background;
 
 for s in xcnd30 xcnd31 xcnd32 xcnd33 xcnd34
 do
@@ -25,9 +31,7 @@ ssh cs4224c@${s}.comp.nus.edu.sg -n "
   --listen-addr=${s}.comp.nus.edu.sg:26267 \
   --http-addr=localhost:8090 \
   --join=xcnd30.comp.nus.edu.sg:26267,xcnd31.comp.nus.edu.sg:26267,xcnd32.comp.nus.edu.sg:26267,xcnd33.comp.nus.edu.sg:26267,xcnd34.comp.nus.edu.sg:26267 \
-  --background;
-
-  exit;
+  --background; &
 "
 done
 
