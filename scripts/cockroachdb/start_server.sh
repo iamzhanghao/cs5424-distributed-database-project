eb87c36f54ed96a4f59d5e40b96f96c1cdb50029
Hao Zhang <zhangghaoo@gmail.com>
2021-10-30 14:42:04 +0800

    Add start_server.sh

---
+++ scripts/cockroachdb/start_server.sh
@@ -0,0 +1,18 @@
+for s in xcnd30 xcnd31 xcnd32 xcnd33 xcnd34
+do
+ssh cs4224c@${s}.comp.nus.edu.sg << EOF
+  killall -9 cockroach;
+  cd cs5424-distributed-database-project/;
+  source .bash_profile;
+  whoami;
+  echo ${s}.comp.nus.edu.sg
+
+  cockroach start \
+  --insecure \
+  --store=cockroach_store/node1 \
+  --listen-addr=${s}.comp.nus.edu.sg:26267 \
+  --http-addr=localhost:8090 \
+  --join=xcnd30.comp.nus.edu.sg:26267,xcnd31.comp.nus.edu.sg:26267,xcnd32.comp.nus.edu.sg:26267,xcnd33.comp.nus.edu.sg:26267,xcnd34.comp.nus.edu.sg:26267 \
+  --background;
+EOF
+done
