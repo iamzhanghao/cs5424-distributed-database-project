f7043587361caddde48d0642f171d40e11d01a0f
Hao Zhang <zhangghaoo@gmail.com>
2021-10-30 14:57:33 +0800

    Fix ssh

--- scripts/cockroachdb/start_server.sh
+++ scripts/cockroachdb/start_server.sh
@@ -1,10 +1,9 @@
 for s in xcnd30 xcnd31 xcnd32 xcnd33 xcnd34
 do
-ssh cs4224c@${s}.comp.nus.edu.sg -n << EOF
+ssh cs4224c@${s}.comp.nus.edu.sg -n "
   killall -9 cockroach;
-EOF
+"
 done
-
 
 rm -rf cockroach_store;
 
@@ -13,22 +12,20 @@ do
 
 echo ${s}
 
-ssh cs4224c@${s}.comp.nus.edu.sg -n << EOF
+ssh cs4224c@${s}.comp.nus.edu.sg -n "
   killall -9 cockroach;
   cd cs5424-distributed-database-project/;
-  source .bash_profile;
+  pwd;
   whoami;
   echo ${s}.comp.nus.edu.sg;
 
-  cockroach start
-  --insecure
-  --store=cockroach_store/node1
-  --listen-addr=${s}.comp.nus.edu.sg:26267
-  --http-addr=localhost:8090
-  --join=xcnd30.comp.nus.edu.sg:26267,xcnd31.comp.nus.edu.sg:26267,xcnd32.comp.nus.edu.sg:26267,xcnd33.comp.nus.edu.sg:26267,xcnd34.comp.nus.edu.sg:26267
+  cockroach start --insecure \
+  --store=cockroach_store/node1 \
+  --listen-addr=${s}.comp.nus.edu.sg:26267 \
+  --http-addr=localhost:8090 \
+  --join=xcnd30.comp.nus.edu.sg:26267,xcnd31.comp.nus.edu.sg:26267,xcnd32.comp.nus.edu.sg:26267,xcnd33.comp.nus.edu.sg:26267,xcnd34.comp.nus.edu.sg:26267 \
   --background;
-
-EOF
+"
 done
 
 cockroach init --insecure --host=xcnd30.comp.nus.edu.sg:26267
