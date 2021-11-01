3a29b22c48f224b2f70b9c9203d05b06e07f9961
Hao Zhang <zhangghaoo@gmail.com>
2021-11-01 21:57:28 +0800

    Add index

--- schema/cockroachdb/add_index_b.sql
+++ schema/cockroachdb/add_index_b.sql
@@ -9,6 +9,9 @@ CREATE INDEX ON customer_tab (c_delivery_cnt);
 
 CREATE INDEX ON district_tab (d_next_o_id);
 CREATE INDEX ON district_tab (d_ytd);
+CREATE INDEX ON district_tab (d_id);
+CREATE INDEX ON district_tab (d_w_id);
+
 
 CREATE INDEX ON item_tab (i_im_id);
 
@@ -19,10 +22,14 @@ CREATE INDEX ON order_line_tab (ol_i_id);
 CREATE INDEX ON order_line_tab (ol_amount);
 CREATE INDEX ON order_line_tab (ol_supply_w_id);
 CREATE INDEX ON order_line_tab (ol_quantity);
+CREATE INDEX ON order_line_tab (ol_w_id,ol_d_id,ol_o_id);
+
 
 CREATE INDEX ON order_tab (o_c_id);
 CREATE INDEX ON order_tab (o_carrier_id);
 CREATE INDEX ON order_tab (o_ol_cnt);
+CREATE INDEX ON order_tab (o_w_id,o_d_id,o_carrier_id);
+
 
 CREATE INDEX ON stock_tab (s_quantity);
 CREATE INDEX ON stock_tab (s_ytd);
