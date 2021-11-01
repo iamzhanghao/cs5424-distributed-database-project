95de34a29ba6b6307e5ebd66684069230c4ed3bd
Hao Zhang <zhangghaoo@gmail.com>
2021-11-02 01:35:26 +0800

    Add stop all cockroach

--- scripts/cockroachdb/start_xcnd30.sh
+++ scripts/cockroachdb/start_xcnd30.sh
@@ -1,15 +1,6 @@
 # Run on xcnd30.comp.nus.edu.sg
 
-for s in xcnd30 xcnd31 xcnd32 xcnd33 xcnd34
-do
-ssh cs4224c@${s}.comp.nus.edu.sg -n "
-  killall -9 cockroach;
-" &
-done
 
-sleep 5;
-
-rm -rf /temp/CS4224C/cockroach_store;
 
 cockroach start --insecure \
   --store=/temp/CS4224C/cockroach_store/xcnd30 \
