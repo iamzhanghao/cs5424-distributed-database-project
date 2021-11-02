fa0f43618bc37745d4804a55d934ec03d556c1bd
Hao Zhang <zhangghaoo@gmail.com>
2021-11-03 00:35:51 +0800

    Add customer

--- src/main/java/clients/utils/Customer.java
+++ src/main/java/clients/utils/Customer.java
@@ -1,5 +1,7 @@
 package clients.utils;
 
+import java.util.Objects;
+
 public class Customer {
 
 
@@ -11,6 +13,19 @@ public class Customer {
         this.w_id = w_id;
         this.d_id = d_id;
         this.c_id = c_id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Customer)) return false;
+        Customer customer = (Customer) o;
+        return w_id == customer.w_id && d_id == customer.d_id && c_id == customer.c_id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(w_id, d_id, c_id);
     }
 
 }
