cb6e568721f9775251a1dce373da4a661b4b1c08
Tianyuan <ftygary@gmail.com>
2021-11-04 15:53:27 +0800

    Removed dummy code

--- src/main/java/clients/Cassandra.java
+++ src/main/java/clients/Cassandra.java
@@ -619,7 +619,6 @@ public class Cassandra {
 
     private static void deliveryTransaction(CqlSession session, int wid, int carrierid) {
         System.out.println("Delivery Txn");
-        final int haha = 2;
         // parallel version
         List<Integer> didRange = IntStream.rangeClosed(1, 10)
                 .boxed().collect(Collectors.toList());
@@ -627,7 +626,6 @@ public class Cassandra {
                 did -> {
 //                    System.out.printf("did: %d ", did);
                     deliveryTransactionUnit(session, wid, carrierid, did);
-                    System.out.println(haha);
                 }
         );
 
